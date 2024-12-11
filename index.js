30860f09535493db5d999a4f21f59179cf784804
Пользователь <i.n.kustov@gmail.com>
2024-12-11 16:39:54 +0500

    database sync success

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Sequelize = require('sequelize');
-const config = require('./config.json');
-const db = require('./models')(Sequelize, config);
+require('dotenv').config();
+const Sequelize = require("sequelize");
+const config = require("./config.json");
+const db = require("./models")(Sequelize, config);
 
+db.sequelize.sync()
+    .then(() => {
+        console.log("Database synced successfully");
+    })
+    .catch((error) => {
+        console.error("Error syncing database:", error);
+    });
+    
 // TODO: запросы к БД
