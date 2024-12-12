ed11e9a1494c2223166142a4cafb39fe99d9bc04
Пользователь <i.n.kustov@gmail.com>
2024-12-12 12:49:41 +0500

    post endpoints

---
+++ handlers/turtles.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+module.exports = (db) => {
+    const router = express.Router();
+
+    router.post("/", async ({ body }, res) => {
+        try {
+            const news = await db.turtles.create(body);
+            res.json(news);
+        } catch (err) {
+            return next({ status: 500, message: err.message });
+        }
+    });
+
+    return router;
+};
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
 const Sequelize = require("sequelize");
 const config = require("./config.json");
 const db = require("./models")(Sequelize, config);
+const turtlesRouter = require("./handlers/turtles")(db);
+const pizzasRouter = require("./handlers/pizzas")(db);
+const weaponsRouter = require("./handlers/weapons")(db);
 
-db.sequelize.sync({force: true})
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/pizzas", pizzasRouter);
+app.use("/api/weapons", weaponsRouter);
+app.use("/api/turtles", turtlesRouter);
+
+app.use((req, res, next) => {
+    next({ status: 404, message: "Not Found" });
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status ?? 500);
+    res.json({ error: err.message });
+});
+
+db.sequelize
+    .sync()
     .then(() => {
         console.log("Database synced successfully");
+        app.listen(3000, () => {
+            console.log(`Server started listening port ${process.env.PORT}`);
+        });
     })
     .catch((error) => {
         console.error("Error syncing database:", error);
     });
-    
-// TODO: запросы к БД
